0f7040ac88db90bbf7c71c47727a1d1b684d0902
VSemikoz <slava.semik@yandex.ru>
2020-09-27 22:13:32 +0300

    add ActivityUtils, ImageUtilsPicasso

---
+++ app/src/main/java/ru/vssemikoz/deezerapp/utils/ActivityUtils.kt
@@ -0,0 +1,13 @@
+package ru.vssemikoz.deezerapp.utils
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+object ActivityUtils {
+    fun addFragmentToActivity(fragmentManager: FragmentManager, fragment: Fragment, frameId: Int) {
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.add(frameId, fragment)
+        transaction.commit()
+    }
+}
