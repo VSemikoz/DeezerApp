dbd74502f342e62b43a4327b18c51e945524fe22
VSemikoz <slava.semik@yandex.ru>
2020-09-27 22:05:21 +0300

    change base components

--- app/src/main/java/ru/vssemikoz/deezerapp/base/di/ApplicationComponent.kt
+++ app/src/main/java/ru/vssemikoz/deezerapp/base/di/ApplicationComponent.kt
@@ -3,16 +3,19 @@ package ru.vssemikoz.deezerapp.base.di
 import dagger.Component
 import ru.vssemikoz.deezerapp.DeezerApplication
 import ru.vssemikoz.deezerapp.data.api.NetworkModule
+import ru.vssemikoz.deezerapp.data.repository.RepositoryModule
+import ru.vssemikoz.deezerapp.features.di.FragmentComponent
 
 @Component(
     modules = [
         ApplicationModule::class,
         NetworkModule::class,
-        AppConfigModule::class
+        AppConfigModule::class,
+        RepositoryModule::class
     ]
 )
 interface ApplicationComponent {
     fun inject(mainApplication: DeezerApplication)
 
-//    fun fragmentComponent(): FragmentComponent
+    fun fragmentComponent(): FragmentComponent
 }
