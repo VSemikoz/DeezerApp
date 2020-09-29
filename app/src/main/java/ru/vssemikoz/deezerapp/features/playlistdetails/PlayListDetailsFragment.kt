12ba43b7bb7f13bbc3b8b4ed52f5b517ea49d105
VSemikoz <slava.semik@yandex.ru>
2020-09-30 01:02:29 +0300

    code refactor

--- app/src/main/java/ru/vssemikoz/deezerapp/features/playlistdetails/PlayListDetailsFragment.kt
+++ app/src/main/java/ru/vssemikoz/deezerapp/features/playlistdetails/PlayListDetailsFragment.kt
@@ -1,14 +1,22 @@
 package ru.vssemikoz.deezerapp.features.playlistdetails
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.playlist_details_frag.view.*
 import ru.vssemikoz.deezerapp.DeezerApplication
 import ru.vssemikoz.deezerapp.R
 import ru.vssemikoz.deezerapp.databinding.PlaylistDetailsFragBinding
+import ru.vssemikoz.deezerapp.features.PlayListDetailsBehavior
+import ru.vssemikoz.deezerapp.features.adapters.TrackAdapter
 import ru.vssemikoz.deezerapp.models.PlayList
 import ru.vssemikoz.deezerapp.models.Track
 import javax.inject.Inject
@@ -17,6 +25,10 @@ import javax.inject.Inject
 class PlayListDetailsFragment : Fragment() {
     @Inject
     lateinit var viewModel: PlayListDetailsViewModel
+    @Inject
+    lateinit var adapter: TrackAdapter
+    private lateinit var recyclerView: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,30 +49,23 @@ class PlayListDetailsFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.playlist_details_frag, container, false)
         val viewDataBinding: PlaylistDetailsFragBinding = PlaylistDetailsFragBinding.bind(view)
         viewDataBinding.vmDetails = viewModel
-        setupCoordinateLayout(view)
+        initRecyclerView(view)
         return view
-    }
-
-    private fun setupCoordinateLayout(view: View) {
-//        val coordinatorLayout = view.findViewById<AppBarLayout>(R.id.appbar)
-//        val behavior: PlayListDetailsBehavior<CollapsingToolbarLayout> = PlayListDetailsBehavior()
-//        behavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY );
-//        behavior.listener = object : SwipeDismissBehavior.OnDismissListener{
-//            override fun onDismiss(view: View?) {
-//                Log.d("setupCoordinateLayout", "onDismiss")
-//            }
-//
-//            override fun onDragStateChanged(state: Int) {
-//                Log.d("setupCoordinateLayout", "onDragStateChanged")
-//            }
-//
-//        }
-//        var params = coordinatorLayout.layoutParams as (CoordinatorLayout.LayoutParams)
-//        params.behavior = behavior
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val trackListUpdateObserver: Observer<List<Track>> = Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        }
+        viewModel.trackList.observe(this, trackListUpdateObserver)
         viewModel.start()
+    }
+
+    private fun initRecyclerView(view: View) {
+        recyclerView = view.rv_tracklist
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/ru/vssemikoz/deezerapp/features/playlistdetails/PlayListDetailsViewModel.kt
+++ app/src/main/java/ru/vssemikoz/deezerapp/features/playlistdetails/PlayListDetailsViewModel.kt
@@ -3,13 +3,14 @@ package ru.vssemikoz.deezerapp.features.playlistdetails
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import androidx.databinding.library.baseAdapters.BR
+import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.vssemikoz.deezerapp.base.BaseUseCase
 import ru.vssemikoz.deezerapp.models.PlayList
 import ru.vssemikoz.deezerapp.models.Track
-import ru.vssemikoz.deezerapp.utils.mappers.DateConverter
+import ru.vssemikoz.deezerapp.utils.mappers.PlayListDateConverter
 import javax.inject.Inject
 
 class PlayListDetailsViewModel @Inject constructor() : BaseObservable() {
@@ -20,40 +21,26 @@ class PlayListDetailsViewModel @Inject constructor() : BaseObservable() {
             notifyPropertyChanged(BR.loading)
         }
 
-    var navigator: PlayListDetailsNavigator? = null
-
     @Inject
     lateinit var getTrackUseCase: BaseUseCase<Observable<List<Track>>, Int>
 
-    var shownTrackList: String = ""
+    var navigator: PlayListDetailsNavigator? = null
+    var trackList: MutableLiveData<List<Track>> = MutableLiveData()
     lateinit var playList: PlayList
+    lateinit var title: String
 
 
     fun start() {
-        getTracks()
-    }
-
-    fun getString(): String {
-        return "${playList.author} - ${DateConverter.map(playList.duration)}"
-    }
-
-    fun getTracks() {
+        title =  "${playList.author} - ${PlayListDateConverter.map(playList.duration)}"
         getTrackUseCase.run(playList.id)
             .doOnSubscribe { loading = true }
             .doOnTerminate { loading = false }
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { message ->
-                    shownTrackList = getTracksName(message)
-                },
+                { message -> trackList.value = message },
                 { error -> error.printStackTrace() },
                 { loading = false }
             )
     }
-
-    fun getTracksName(trackList: List<Track>): String =
-        trackList.joinToString(separator = "\n") { "${it.title} -  ${DateConverter.map(it.duration)}" }
-
-    fun getEmptyName(): String = "Loading data"
 }
