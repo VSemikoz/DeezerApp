1b3cc72d351446f38fd14d3afb8c14ade9541fbc
VSemikoz <slava.semik@yandex.ru>
2020-09-29 19:31:49 +0300

    correct adapter

--- app/src/main/java/ru/vssemikoz/deezerapp/features/PlayListAdapter.kt
+++ app/src/main/java/ru/vssemikoz/deezerapp/features/PlayListAdapter.kt
@@ -48,9 +48,12 @@ class PlayListAdapter @Inject constructor() : BaseAdapter<PlayList>() {
         }
 
         override fun onBind(item: PlayList, listener: OnRecyclerItemClickListener?) {
-            playListName.text = item.title
-            ImageUtilsPicasso.setImageByUrl(item.cover, playListImage)
+            playListName.text = item.title.reduceString(15)
+            ImageUtilsPicasso.setImageByUrl(playListImage, item.cover)
         }
+
+        private fun String.reduceString(reduceLength: Int): String = if (reduceLength > length) this
+        else "${substring(0, reduceLength)}..."
 
     }
 
