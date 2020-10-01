a07f30b0b23e061fd354400b8f682ca6f8394156
VSemikoz <slava.semik@yandex.ru>
2020-10-02 00:19:42 +0300

    add limit value into config

--- app/src/main/java/ru/vssemikoz/deezerapp/data/api/DeezerApi.kt
+++ app/src/main/java/ru/vssemikoz/deezerapp/data/api/DeezerApi.kt
@@ -3,13 +3,20 @@ package ru.vssemikoz.deezerapp.data.api
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 import ru.vssemikoz.deezerapp.models.DeezerApiPlayListsResponse
 import ru.vssemikoz.deezerapp.models.DeezerApiTrackResponse
 
 interface DeezerApi {
     @GET("user/{user_id}/playlists")
-    fun getUserPlayList(@Path("user_id") id: Int): Observable<DeezerApiPlayListsResponse>
+    fun getUserPlayList(
+        @Path("user_id") id: String,
+        @Query("limit") limit: Int
+    ): Observable<DeezerApiPlayListsResponse>
 
     @GET("playlist/{playlist_id}/tracks")
-    fun getTrackFromPlayList(@Path("playlist_id") id: Int): Observable<DeezerApiTrackResponse>
+    fun getTrackFromPlayList(
+        @Path("playlist_id") id: String,
+        @Query("limit") limit: Int
+    ): Observable<DeezerApiTrackResponse>
 }
